5fa9ab21d5c7fe4d77e38495f49bb5508c9dac8e
NhutLinh-1007 <linhlove00b@gmail.com>
2024-11-08 11:55:36 +0700

    1

--- App/Controllers/Client/ProductController.php
+++ App/Controllers/Client/ProductController.php
@@ -41,29 +41,56 @@ class ProductController
         $products = [
             [
                 'id' => 1,
-                'name' => 'Product 1',
-                'description' => 'Description Product 1',
-                'price' => 100000,
-                'discount_price' => 10000,
-                'image' => 'product.jpg',
+                'name' => 'Bàn chải điện Xiaomi',
+                'description' => 'Nhập khẩu chính hãng, Bảo hành chính hãng, Động cơ mạnh mẽ',
+                'price' => 300000,
+                'discount_price' => 20000,
+                'image' => 'ban-chai-dien-xiaomi-mijia-smart-sonic-t500-thumbnail-300x300.jpg',
                 'status' => 1
             ],
             [
                 'id' => 2,
-                'name' => 'Product 2',
-                'description' => 'Description Product 2',
-                'price' => 200000,
-                'discount_price' => 20000,
-                'image' => 'product.jpg',
+                'name' => 'Bếp lẩu nướng đa năng',
+                'description' => 'Nhập khẩu chính hãng, Bảo hành chính hãng, Nấu nướng đa năng',
+                'price' => 1000000,
+                'discount_price' => 50000,
+                'image' => 'bep-lau-nuong-da-nang-bear-dhg-c40d5-thumbnail.jpg',
                 'status' => 1
             ],
             [
                 'id' => 3,
-                'name' => 'Product 3',
-                'description' => 'Description Product 3',
+                'name' => 'Bộ kiềm vệ sinh móng tay',
+                'description' => 'Nhập khẩu chính hãng, Bảo hành chính hãng,Sử dụng bền bỉ',
+                'price' => 150000,
+                'discount_price' => 30000,
+                'image' => 'bo-lay-ray-tai-co-den-kiem-bam-mong-2in1-xiaomi-huohou-thumbnail-300x300.jpg',
+                'status' => 1
+            ],
+            [
+                'id' => 3,
+                'name' => 'Bộ sạc 5 cổng USB',
+                'description' => 'Nhập khẩu chính hãng, Bảo hành chính hãng, Sử dụng bền bỉ',
                 'price' => 300000,
                 'discount_price' => 30000,
-                'image' => 'product.jpg',
+                'image' => 'bo-sac-5-cong-USB-Anker-01-300x300.jpg',
+                'status' => 1
+            ],
+            [
+                'id' => 3,
+                'name' => 'Chuông cửa thông minh Xiaomi',
+                'description' => 'Nhập khẩu chính hãng, Bảo hành chính hãng',
+                'price' => 1200000,
+                'discount_price' => 100000,
+                'image' => 'chuong-cua-thong-minh-xiaomi-cat-eye-lsc-m01-thumbnail-600x600.jpg',
+                'status' => 1
+            ],
+            [
+                'id' => 3,
+                'name' => 'Máy nấu ăn đa năng',
+                'description' => 'Nhập khẩu chính hãng, Bảo hành chính hãng, Nấu nướng đa năng, Sử dụng bền bỉ',
+                'price' => 800000,
+                'discount_price' => 30000,
+                'image' => 'may-nau-an-da-nang-fanlai-fl-m1301-thumbnail-300x300.jpg',
                 'status' => 1
             ],
 
@@ -96,7 +123,5 @@ class ProductController
         Detail::render($data);
         Footer::render();
     }
-    public static function getProductByCategory($id)
-    {
-    }
+    public static function getProductByCategory($id) {}
 }
