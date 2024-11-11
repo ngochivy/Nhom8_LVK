c0042d2078e3e505dc393795fc4744caa827d15b
Zync <143595408+ngochivy@users.noreply.github.com>
2024-11-11 15:33:07 +0700

    update font

--- App/Views/Client/Home.php
+++ App/Views/Client/Home.php
@@ -28,7 +28,8 @@ class Home extends BaseView
             <!-- Google Web Fonts -->
             <link rel="preconnect" href="https://fonts.gstatic.com">
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
             <!-- Font Awesome -->
             <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
